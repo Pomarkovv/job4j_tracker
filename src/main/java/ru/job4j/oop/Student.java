5063b181dc03d1b734b50cb484732e84585cf0e0
Ivan Pomarkov <pomarkovivann2002@outlook.com>
2022-06-12 18:32:36 +0700

    1.5. Поля объекта [#166083]

--- src/main/java/ru/job4j/oop/Student.java
+++ src/main/java/ru/job4j/oop/Student.java
@@ -1,9 +1,10 @@
 package ru.job4j.oop;
 
 public class Student {
-    public void music (String lyrics) {
+    public void music(String lyrics) {
         System.out.println("I can sing a song:" + lyrics);
     }
+
     public static void main(String[] args) {
         Student petya = new Student();
         String song = "I believe, I can fly";
