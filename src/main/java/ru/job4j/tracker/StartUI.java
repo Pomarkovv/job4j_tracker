c07f4f5d20053430a7024080f41398b158a734d5
Ivan Pomarkov <pomarkovivann2002@outlook.com>
2022-06-15 12:39:57 +0700

    9. toString [#310018]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -11,5 +11,7 @@ public class StartUI {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
         String currentDateTimeFormat = time.format(formatter);
         System.out.println("Текущие дата и время после форматирования: " + currentDateTimeFormat);
+        Item example = new Item();
+        System.out.println(example);
     }
 }
