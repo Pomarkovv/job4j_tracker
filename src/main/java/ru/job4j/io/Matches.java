bb0bc79dafa53db37612c077a1cbef8f85b26863
Ivan Pomarkov <pomarkovivann2002@outlook.com>
2022-06-15 18:50:41 +0700

    Исправление в Matches

--- src/main/java/ru/job4j/io/Matches.java
+++ src/main/java/ru/job4j/io/Matches.java
@@ -12,7 +12,6 @@ public class Matches {
             String player = turn ? "Первый игрок" : "Второй игрок";
             System.out.println(player + " введите число от 1 до 3:");
             int matches = Integer.parseInt(input.nextLine());
-            turn = !turn;
             if (matches > 0 && matches <= 3 && matches <= count) {
                 count = count - matches;
                 turn = !turn;
