174ee410653f4532ac24ea9d813816f113f15e46
Ivan Pomarkov <pomarkovivann2002@outlook.com>
2022-06-15 09:01:26 +0700

    5. Tracker - хранилище [#396]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -21,5 +23,28 @@ public class Tracker {
             }
         }
         return rsl;
+    }
+
+    public Item[] findAll() {
+        Item[] copy = new Item[items.length];
+        int size = 0;
+        for (int i = 0; i < items.length; i++) {
+            Item el = items[i];
+            if (el != null) {
+                copy[size] = el;
+                size++;
+            }
+        } return Arrays.copyOf(copy, size);
+    }
+
+    public Item[] findByName(String key) {
+        Item[] copy = new Item[size];
+        int i = 0;
+        for (int index = 0; index < size; index++) {
+            if (key.equals(items[index].getName())) {
+                copy[i] = items[index];
+                i++;
+            }
+        } return Arrays.copyOf(copy, i);
     }
 }
