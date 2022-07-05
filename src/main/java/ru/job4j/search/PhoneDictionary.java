5cfd1a5ee66495ea43b779ab47109dddabb238c6
Ivan Pomarkov <pomarkovivann2002@outlook.com>
2022-07-05 10:35:53 +0700

    исправление в phoneDictionary

--- src/main/java/ru/job4j/search/PhoneDictionary.java
+++ src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -13,9 +13,12 @@ public class PhoneDictionary {
     public ArrayList<Person> find(String key) {
         ArrayList<Person> result = new ArrayList<>();
         for (Person pers : persons) {
-            if (pers.getPhone().contains(key) ||
-                    pers.getAddress().contains(key) ||
-                    pers.getName().contains(key) ||
+            if (pers.getPhone().contains(key)
+                    ||
+                    pers.getAddress().contains(key)
+                    ||
+                    pers.getName().contains(key)
+                    ||
                     pers.getSurname().contains(key)) {
                 result.add(pers);
             }
