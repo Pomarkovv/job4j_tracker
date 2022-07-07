4af6fec857aaa89430ec06a26c5ac1fc94aeab07
Ivan Pomarkov <pomarkovivann2002@outlook.com>
2022-07-07 12:20:53 +0700

    fix

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -3,6 +3,7 @@ package ru.job4j.tracker;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class StartUI {
