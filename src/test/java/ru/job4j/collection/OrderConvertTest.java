26870cf0e7be56b7581bd5f6d22e127419c820f1
Ivan Pomarkov <pomarkovivann2002@outlook.com>
2022-07-16 14:34:11 +0700

    справление в OrderConvertTest

--- src/test/java/ru/job4j/collection/OrderConvertTest.java
+++ src/test/java/ru/job4j/collection/OrderConvertTest.java
@@ -25,6 +25,6 @@ public class OrderConvertTest {
         orders.add(new Order("3sfe", "Dress"));
         orders.add(new Order("3sfe", "Dress"));
         HashMap<String, Order> map = OrderConvert.process(orders);
-        assertTrue(map.size() < orders.size());
+        assertEquals(1, map.size());
     }
 }
