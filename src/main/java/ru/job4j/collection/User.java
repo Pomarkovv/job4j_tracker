ed41281aea7d2a7bc38c355636369f5a2b362e93
Ivan Pomarkov <pomarkovivann2002@outlook.com>
2022-07-20 09:58:59 +0700

    исправление в User

--- src/main/java/ru/job4j/collection/User.java
+++ src/main/java/ru/job4j/collection/User.java
@@ -14,10 +14,11 @@ public class User implements Comparable<User> {
 
     @Override
     public int compareTo(User o) {
-        if (this.name.compareTo(o.name) == 0) {
+        int name = this.name.compareTo(o.name);
+        if (name == 0) {
             return Integer.compare(this.age, o.age);
         }
-        return this.name.compareTo(o.name);
+        return name;
     }
 
     @Override
