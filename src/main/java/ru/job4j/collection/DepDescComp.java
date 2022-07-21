90f86e830fe6b3067642238ce63f00189fc2aa0e
Ivan Pomarkov <pomarkovivann2002@outlook.com>
2022-07-21 20:29:19 +0700

    2. Отсортировать департаменты [#34352]

---
+++ src/main/java/ru/job4j/collection/DepDescComp.java
@@ -0,0 +1,17 @@
+package ru.job4j.collection;
+
+import java.util.Comparator;
+
+public class DepDescComp implements Comparator<String> {
+    @Override
+    public int compare(String o1, String o2) {
+        String[] elo1 = o1.split("/");
+        String[] elo2 = o2.split("/");
+        int result = elo2[0].compareTo(elo1[0]);
+        if (result != 0) {
+            return result;
+        } else {
+            return o1.compareTo(o2);
+        }
+    }
+}
